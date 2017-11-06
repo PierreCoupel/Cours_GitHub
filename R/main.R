cf5d72686568d732c9d907becce04ddd2d77b2be
PierreCoupel <pierre.coupel@takuvik.ulaval.ca>
2017-11-06 10:37:45 +0800

    Creation projet

---
+++ R/main.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+
+rm(list = ls())
+
+## Processing
+source("R/process_temperature.R")
+
+## Graphs
+source("R/plot_temperature.R")
---
+++ R/process_temperature.R
@@ -0,0 +1,6 @@
+
+df = read.csv("data/raw/T_Antalya_EM.csv")
+
+df = gather(df, mois, temperature, -tplace, -year)
+
+write_csv(df,"data/clean/T_Antalya_EM.csv")
